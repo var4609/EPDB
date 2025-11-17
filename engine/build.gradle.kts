8a8aea67c793e9718e8f257b6de7026d1ae41dbf
var4609 <varun.patni1@gmail.com>
2025-11-17 09:37:14 -0600

    Centralize build behavior into buildSrc

--- engine/build.gradle.kts
+++ engine/build.gradle.kts
@@ -1,22 +1,8 @@
 plugins {
-    java
-    jacoco
+    id("java-conventions")
 }
 
 dependencies {
     implementation(project(":buffer"))
     implementation(project(":storage"))
-
-    testImplementation(libs.junit)
-    testImplementation(libs.mockito)
-}
-
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
-}
-
-jacoco {
-    toolVersion = "0.8.14" // Use a recent version
 }
